9e2e6667aa6e81c7ee4e7d846b947187714ef678
Venelin Stoykov <vkstoykov@gmail.com>
2013-02-06 22:58:13 +0200

    Add 'replace' template filter to builtins

--- webserver/templatetags/extra_builtins.py
+++ webserver/templatetags/extra_builtins.py
@@ -6,3 +6,9 @@ register = Library()
 @register.filter
 def split(val, splitter):
 	return val.split(splitter)
+
+
+@register.filter
+def replace(val, args):
+	search_string, replacement = args.split(',')
+	return val.replace(search_string, replacement)
