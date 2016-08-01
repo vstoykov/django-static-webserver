53c9da4cb96c26b4011dc1f13d54061a2eb32fc7
Venelin Stoykov <venelin@magicsolutions.bg>
2016-08-01 11:43:18 +0300

    PEP8

--- webserver/views.py
+++ webserver/views.py
@@ -4,13 +4,17 @@ from django.conf import settings
 
 
 def render_template(request, path):
-	if path and settings.APPEND_SLASH and not path.endswith('/'):
-		return redirect('/%s/' % path)
-	templates = [
-			'%s.html' % path.rstrip('/'),
-			'%sindex.html' % path
-			]
-	try:
-		return render(request, templates, {'request': request})
-	except TemplateDoesNotExist:
-		raise Http404('/%s can not be found' % path)
+    if path and settings.APPEND_SLASH and not path.endswith('/'):
+        return redirect('/%s/' % path)
+
+    templates = [
+        '%s.html' % path.rstrip('/'),
+        '%sindex.html' % path
+    ]
+
+    try:
+        return render(request, templates, {'request': request})
+    except TemplateDoesNotExist:
+        if settings.DEBUG:
+            raise
+        raise Http404('/%s can not be found' % path)
